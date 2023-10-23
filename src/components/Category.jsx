2fe7777aa7775fb1da2895e15dc3d28a03ad260f
JanithKE <49825461+JanithKE@users.noreply.github.com>
2023-10-24 02:47:25 +0530

    category section completed

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,17 +5,21 @@ const Category = () => {
     console.log(categories)
   return (
 
-    <div>
+    <div className='max-w-[1640px] m-auto px-4 py-12'>
 
-      <h1>Top Rated Menu Items</h1>
+      <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
 
       {/*categories */}
 
-      <div>
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
+
         {categories.map((item,index)=> (
-            <div key={index}>
-                <h2>{item.name}</h2>
-                <img src={item.image} alt={item.name}/>
+            <div key={index} className='rounded-lg py-4 flex justify-between items-center bg-gray-100'>
+
+                <h2 className='font-bold sm:text-xl ml-5'>{item.name}</h2>
+
+                <img src={item.image} alt={item.name} className='w-20'/>
+
             </div>
         ))}
       </div>
